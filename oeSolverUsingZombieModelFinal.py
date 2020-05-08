05d81d7c15a8c812ddbd70453448669abc76fa2e
Saumil Thakore <saumilkt@gmail.com>
2020-05-08 01:10:25 -0700

    bugfix

--- oeSolverUsingZombieModelFinal.py
+++ oeSolverUsingZombieModelFinal.py
@@ -26,30 +26,30 @@ alpha_a1 = weight.alpha_a_weightage(int(I), alpha_a_base) # adult virulence (par
 alpha_a2 = weight.alpha_a_weightage(int(I), alpha_a_base) # adult virulence (parametric)
 """
 #parametrics
-alpha_i = weight.alpha_a_weightage(int(I), alpha_a_base) # adult virulence (parametric)
-alpha_i1 = weight.alpha_a_weightage(int(I), alpha_a_base) # adult virulence (parametric)
-alpha_i2 = weight.alpha_a_weightage(int(I), alpha_a_base) # adult virulence (parametric)
+alpha_i = weight.alpha_a_weightage(int(I), alpha_i_base) # adult virulence (parametric)
+alpha_i1 = weight.alpha_a_weightage(int(I), alpha_i_base) # adult virulence (parametric)
+alpha_i2 = weight.alpha_a_weightage(int(I), alpha_i_base) # adult virulence (parametric)
 
 # solve the system dy/dt = f(y, t)
 def f(N, t):
      Ni = N[0]
      # the model equations (see Munz et al. 2009)
      f0 = Ni*(a-bi-bd*Ni)-I*((1+a*(v*alpha_i-1))+alpha_a)
-     print(alpha_a)
+     print(alpha_i)
      return [f0]
 
 def f1(N, t):
      Ni = N[0]
      # the model equations (see Munz et al. 2009)
      f0 = Ni*(a-bi-bd*Ni)-I*((1+a*(v*alpha_i1-1))+alpha_a)
-     print(alpha_a)
+     print(alpha_i1)
      return [f0]
 
 def f2(N, t):
      Ni = N[0]
      # the model equations (see Munz et al. 2009)
      f0 = Ni*(a-bi-bd*Ni)-I*((1+a*(v*alpha_i2-1))+alpha_a)
-     print(alpha_a)
+     print(alpha_i2)
      return [f0]
 
 # solve the DEs
@@ -64,9 +64,9 @@ S2 = soln2[:, 0]
 
 # plot results
 plt.figure()
-plt.plot(t, S, label='alpha_a= '+str(alpha_a))
-plt.plot(t, S1, label='alpha_a= '+str(alpha_a1))
-plt.plot(t, S2, label='alpha_a= '+str(alpha_a2))
+plt.plot(t, S, label='alpha_i= '+str(alpha_i))
+plt.plot(t, S1, label='alpha_i= '+str(alpha_i1))
+plt.plot(t, S2, label='alpha_i= '+str(alpha_i2))
 plt.xlabel('Timesteps')
 plt.ylabel('Population')
 plt.title('Effect of differring Virulence levels on Monarch Butterfly Population over Time')
