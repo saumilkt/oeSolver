8b2f4e5834b2666c6abe392228ac02d66e3ed2e2
Saumil Thakore <saumilkt@gmail.com>
2020-05-08 01:15:14 -0700

    added control functionality

--- oeSolverUsingZombieModelFinal.py
+++ oeSolverUsingZombieModelFinal.py
@@ -52,13 +52,22 @@ def f2(N, t):
      print(alpha_i2)
      return [f0]
 
+def fcontrol(N, t): # remove if not implementing parametric study
+     Ni = N[0]
+     # the model equations (see Munz et al. 2009)
+     f0 = Ni*(a-bi-bd*Ni)-I*((1+a*(v*alpha_i_base-1))+alpha_a)
+     print(alpha_i_base)
+     return [f0]
+
 # solve the DEs
 soln = odeint(f, y0, t)
 soln1 = odeint(f1, y0, t)
 soln2 = odeint(f2, y0, t)
+solnC = odeint(fcontrol, y0, t)
 S = soln[:, 0]
 S1 = soln1[:, 0]
 S2 = soln2[:, 0]
+SC = solnC[:, 0]
 
 
 
@@ -67,6 +76,7 @@ plt.figure()
 plt.plot(t, S, label='alpha_i= '+str(alpha_i))
 plt.plot(t, S1, label='alpha_i= '+str(alpha_i1))
 plt.plot(t, S2, label='alpha_i= '+str(alpha_i2))
+plt.plot(t, S2, label='Control: alpha_i_base= '+str(alpha_i_base))
 plt.xlabel('Timesteps')
 plt.ylabel('Population')
 plt.title('Effect of differring Virulence levels on Monarch Butterfly Population over Time')
