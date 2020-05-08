ddeb055b83d464ae87a54eb3e610113564ff5307
Saumil Thakore <saumilkt@gmail.com>
2020-05-08 01:08:47 -0700

    bugfix

--- oeSolverUsingZombieModelFinal.py
+++ oeSolverUsingZombieModelFinal.py
@@ -17,8 +17,8 @@ bi = 0.8000  # Desity-independent death percent (per day)
 bd = 0.1000  # Desity-dependent death percent  (per day)
 I = .01*N0  # resurect percent (per day)
 v = .9000  # rate of maternal transmission
-alpha_i_base = 0.2 # pre-adult virulence
-alpha_a_base = 0.1 # adult virulence (standard)
+alpha_i_base = 0.2 # pre-adult virulence (standard) ***rename to alpha_a_base when parametric study in effect
+alpha_a = 0.1 # adult virulence (standard) ***rename to alpha_a_base when parametric study in effect
 
 """#parametrics
 alpha_a = weight.alpha_a_weightage(int(I), alpha_a_base) # adult virulence (parametric)
@@ -34,21 +34,21 @@ alpha_i2 = weight.alpha_a_weightage(int(I), alpha_a_base) # adult virulence (par
 def f(N, t):
      Ni = N[0]
      # the model equations (see Munz et al. 2009)
-     f0 = Ni*(a-bi-bd*Ni)-I*((1+a*(v*alpha_i-1))+alpha_a_base)
+     f0 = Ni*(a-bi-bd*Ni)-I*((1+a*(v*alpha_i-1))+alpha_a)
      print(alpha_a)
      return [f0]
 
 def f1(N, t):
      Ni = N[0]
      # the model equations (see Munz et al. 2009)
-     f0 = Ni*(a-bi-bd*Ni)-I*((1+a*(v*alpha_i1-1))+alpha_a_base)
+     f0 = Ni*(a-bi-bd*Ni)-I*((1+a*(v*alpha_i1-1))+alpha_a)
      print(alpha_a)
      return [f0]
 
 def f2(N, t):
      Ni = N[0]
      # the model equations (see Munz et al. 2009)
-     f0 = Ni*(a-bi-bd*Ni)-I*((1+a*(v*alpha_i2-1))+alpha_a_base)
+     f0 = Ni*(a-bi-bd*Ni)-I*((1+a*(v*alpha_i2-1))+alpha_a)
      print(alpha_a)
      return [f0]
 
